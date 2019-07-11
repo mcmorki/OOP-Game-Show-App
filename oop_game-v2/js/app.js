44f3f9067e25588fb5ea3afffccb7b9734ac414b
mcmorki <Kiyana.mcmorris@verizon.com>
2019-07-11 14:00:32 -0400

    7/11/2019
    7/11/2019 2:00

--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -1,5 +1,17 @@
+let currentGame;
+let currentPhrase;
+let phrases = [
+    "Doris Longwing",
+    "Blue Morpho",
+    "Mitchell\'s Satyr",
+    "Banded Peacock",
+    "Ceylon Rose",
+    "Island Marble",
+    "Luzon Giant Swallowtail",
+    "glasswinged butterfly",
+    "Jamaican Giant Swallowtail"]
 
-let game = new Game();
+let game = new Game(0, phrases);
 // resetDisplay(): this function hides the start screen overlay.
 function resetDisplay() {
     $('#overlay').hide();
@@ -7,9 +19,15 @@ function resetDisplay() {
     $('.key').prop("className", "key").css("background", "#D2D2D2");
     $('.tries').show();
 }
-
+// Add an event listener to the "Start Game" button which calls the resetDisplay() function, creates a new Game object, and starts the game.
 $('#btn__reset').click(function () {
     resetDisplay();
-    game.startGame();
-    game.handleInteraction();
+    gamePhrase = game.startGame();
 });
+// markButton(): this function is called when a player selects a letter. It disables the button on the onscreen keyboard and calls the handleInteraction() method of the Game class.
+// Add event listeners to each of the keyboard buttons, so that clicking a button calls the markButton() function.
+function markButton() {
+    $(this).attr('disabled', 'disabled');
+    game.handleInteraction();
+}
+markButton();
